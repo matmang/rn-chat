9e543a40970f1832220c2d3c8d74b7b970fae93c
matmang <64837797+matmang@users.noreply.github.com>
2021-07-16 05:08:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import {StatusBar} from 'react-native';
 import {ThemeProvider} from 'styled-components/native'
 import {theme} from './theme'
 import Navigation from './navigations';
+import { UserProvider, ProgressProvider } from './contexts';
 
 const App = () => {
     return (
         <ThemeProvider theme = {theme}>
-            <StatusBar 
-                backgroundColor={theme.background}
-                barStyle="dark-content"
-            />
-            <Navigation />
+            <ProgressProvider>
+                <UserProvider>
+                    <StatusBar 
+                        backgroundColor={theme.background}
+                        barStyle="dark-content"
+                    />
+                    <Navigation />
+                </UserProvider>
+            </ProgressProvider>
         </ThemeProvider>
-    )
+    );
 }
 
 export default App;
