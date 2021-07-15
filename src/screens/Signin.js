cecfd3c17a7ccb4b07d58fab2b6c493649cda6e4
matmang <64837797+matmang@users.noreply.github.com>
2021-07-15 20:47:46 +0900

    Create Signin.js

---
+++ src/screens/Signin.js
@@ -0,0 +1,66 @@
+import React, {useContext, useState, useRef} from 'react';
+import { ThemeContext } from 'styled-components/native';
+import styled from 'styled-components/native';
+import { Button, Image, Input } from '../components';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const Container = styled.View`
+flex: 1;
+justify-content: center;
+align-items: center;
+background-color: ${({theme}) => theme.background};
+padding: 0 20px;
+padding-top: ${({insets: {top}}) => top}px;
+padding-bottom: ${({insets: {bottom}}) => bottom}px;
+`;
+
+const LOGO = 'https://firebasestorage.googleapis.com/v0/b/rn-chat-6e02e.appspot.com/o/logo.png?alt=media'
+
+const Signin = ({navigation}) => {
+    const insets = useSafeAreaInsets();
+    const theme = useContext(ThemeContext);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const refPassword = useRef(null);
+
+    const _handleSigninBtnPress = () => {
+        console.log('signin');
+    }
+
+    return(
+        <Container insets={insets}>
+            <Image url= {LOGO} />
+            <Input
+                label="Email"
+                placeholder="Email"
+                returnKeyType="next"
+                value={email}
+                onChangeText={setEmail}
+                onSubmitEditing={() => refPassword.current.focus()}
+            />
+            <Input
+                ref={refPassword}
+                label="Password"
+                placeholder="Password"
+                returnKeyType="done"
+                value={password}
+                onChangeText={setPassword}
+                isPassword={true}
+                onSubmitEditing={_handleSigninBtnPress}
+            />
+            <Button
+                title="Signup"
+                onPress={_handleSigninBtnPress}
+            />
+            <Button
+                title="or sign up"
+                onPress={() => navigation.navigate('Signup')}
+                containerStyle={{marginTop: 0, backgroundColor: 'transparent'}}
+                textStyle={{color: theme.btnTextLink, fontSize: 18}}
+            />
+        </Container>
+    );
+}
+
+export default Signin;
