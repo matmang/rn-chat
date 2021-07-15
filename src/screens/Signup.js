7d0954fed6e9ea9eb4b2227f04072feb1cbee535
matmang <64837797+matmang@users.noreply.github.com>
2021-07-15 21:12:18 +0900

    Update Signup.js

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,23 +1,78 @@
-import React from 'react';
+import React, { useState, useRef} from 'react';
 import styled from 'styled-components/native';
+import { Button, Image, Input } from '../components';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const Container = styled.View`
 flex: 1;
 justify-content: center;
 align-items: center;
 background-color: ${({theme}) => theme.background};
-padding: 0 20px;
+padding: 50px 20px;
 `;
-const StyledText = styled.Text`
-font-size: 30px;
-color: #111111;
-`
 
-const Signup = () => {
+const Signup = ({navigation}) => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    
+    const refEmail = useRef(null);
+    const refPassword = useRef(null);
+    const refPasswordConfirm = useRef(null);
+
+    const _handleSignupBtnPress = () => {
+        console.log('signup');
+    }
+
     return(
+        <KeyboardAwareScrollView extraScrollHeight={20}>
         <Container>
-            <StyledText>Signup</StyledText>
+            <Image />
+            <Input
+                label="Name"
+                placeholder="Name"
+                returnKeyType="next"
+                value={name}
+                onChangeText={setName}
+                onSubmitEditing={() => refEmail.current.focus()}
+            />
+            <Input
+                ref={refEmail}
+                label="Email"
+                placeholder="Email"
+                returnKeyType="next"
+                value={email}
+                onChangeText={setEmail}
+                onSubmitEditing={() => refPassword.current.focus()}
+            />
+            <Input
+                ref={refPassword}
+                label="Password"
+                placeholder="Password"
+                returnKeyType="next"
+                value={password}
+                onChangeText={setPassword}
+                isPassword={true}
+                onSubmitEditing={() => refPasswordConfirm.current.focus()}
+            />
+            <Input
+                ref={refPasswordConfirm}
+                label="Password Confirm"
+                placeholder="Password"
+                returnKeyType="done"
+                value={passwordConfirm}
+                onChangeText={setPasswordConfirm}
+                isPassword={true}
+                onSubmitEditing={_handleSignupBtnPress}
+            />
+            <Button
+                title="Signup"
+                onPress={_handleSignupBtnPress}
+            />
         </Container>
+        </KeyboardAwareScrollView>
     );
 }
 
