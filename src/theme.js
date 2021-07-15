f9cbb7c8d92c66898cf9803abc1b3c65770f8f80
matmang <64837797+matmang@users.noreply.github.com>
2021-07-16 05:09:10 +0900

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -3,21 +3,28 @@ const Colors = {
     black: '#111111',
     main: '#3679fe',
     grey_0: '#d5d5d5',
-    grey_1: '#a6a6a6'
+    grey_1: '#a6a6a6',
+    red: '#e84118',
 };
 
 export const theme = {
     background: Colors.white,
     text: Colors.black,
+    errorText: Colors.red,
 
     btnBackground: Colors.main,
     btnTitle: Colors.white,
     btnTextLink: Colors.main,
 
     imgBackground: Colors.grey_0,
+    imgBtnBackground: Colors.grey_1,
+    imgBtnIcon: Colors.white,
 
     inputBackground: Colors.white,
     inputLabel: Colors.grey_1,
     inputPlaceholder: Colors.grey_1,
     inputBorder: Colors.grey_1,
+
+    spinnerBackground: Colors.black,
+    spinnerIndicator: Colors.white,
 }
