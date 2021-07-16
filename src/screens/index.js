678ad6eb9abbe13d4492e947f59d1dba438a43eb
matmang <64837797+matmang@users.noreply.github.com>
2021-07-16 22:21:26 +0900

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,8 @@
 import Signin from './Signin';
 import Signup from './Signup';
 import Profile from './Profile';
+import Channel from './Channel';
+import ChannelCreation from './ChannelCreation';
+import ChannelList from './ChannelList';
 
-export {Signin, Signup, Profile};
+export {Signin, Signup, Profile, Channel, ChannelCreation, ChannelList};
